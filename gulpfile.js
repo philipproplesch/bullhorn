34def53cb2aa60a55eea3a79f7bb63d75e7fd9ad
Philip Proplesch <philip@proplesch.de>
2014-12-06 18:08:56 +0100

    Restructured project

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('sass', function () {
-  gulp.src('./public/styles/**/*.scss')
+  gulp.src('./src/public/styles/**/*.scss')
   .pipe(plugins.sourcemaps.init())
   .pipe(plugins.sass())
   .pipe(plugins.sourcemaps.write())
-  .pipe(gulp.dest('./public/styles'));
+  .pipe(gulp.dest('./src/public/styles'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./public/styles/**/*.scss', ['sass']);
+  gulp.watch('./src/public/styles/**/*.scss', ['sass']);
 });
+
+gulp.task('run', plugins.shell.task([
+  'node_modules/node-webkit-builder/bin/nwbuild --run ./src'
+]));
+
+gulp.task('build-win', plugins.shell.task([
+  'node_modules/node-webkit-builder/bin/nwbuild -p win ./src'
+]));
+
+gulp.task('build-osx', plugins.shell.task([
+  'node_modules/node-webkit-builder/bin/nwbuild -p osx ./src'
+]));
