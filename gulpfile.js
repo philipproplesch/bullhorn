3e5269cc2971cfc1988a5a97df6ecf48626f7dd3
Philip Proplesch <philip@proplesch.de>
2014-12-06 21:26:43 +0100

    Fixed cross platform issues in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
+var os = require('os');
+
 gulp.task('sass', function () {
   gulp.src('./src/public/styles/**/*.scss')
   .pipe(plugins.sourcemaps.init())
@@ -13,14 +15,20 @@ gulp.task('watch', function() {
   gulp.watch('./src/public/styles/**/*.scss', ['sass']);
 });
 
-gulp.task('run', plugins.shell.task([
-  'node_modules/node-webkit-builder/bin/nwbuild --run ./src'
-]));
+gulp.task('run', plugins.shell.task(
+  os.platform() === 'win32' ?
+    'node node_modules/node-webkit-builder/bin/nwbuild --run ./src' :
+    'node_modules/node-webkit-builder/bin/nwbuild --run ./src'
+));
 
-gulp.task('build-win', plugins.shell.task([
-  'node_modules/node-webkit-builder/bin/nwbuild -p win ./src'
-]));
+gulp.task('build-win', plugins.shell.task(
+  os.platform() === 'win32' ?
+    'node node_modules/node-webkit-builder/bin/nwbuild -p win ./src' :
+    'node_modules/node-webkit-builder/bin/nwbuild -p win ./src'
+));
 
-gulp.task('build-osx', plugins.shell.task([
-  'node_modules/node-webkit-builder/bin/nwbuild -p osx ./src'
-]));
+gulp.task('build-osx', plugins.shell.task(
+  os.platform() === 'win32' ?
+    'node node_modules/node-webkit-builder/bin/nwbuild -p osx ./src' :
+    'node_modules/node-webkit-builder/bin/nwbuild -p osx ./src'
+));
