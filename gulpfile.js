303020dafe6428c9f808ef19e9860e118e22a659
Philip Proplesch <philip@proplesch.de>
2014-12-09 20:39:27 +0100

    Added Karma and configured Mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
+var runSequence = require('run-sequence');
+
+function executeNodeCommand(cmd) {
+  var platform = require('os').platform();
+
+  if (platform === 'win32') {
+    cmd = 'node ' + cmd;
+  }
+
+  return plugins.shell.task(cmd);
+}
+
 gulp.task('lint', function() {
-  return gulp.src('./src/scripts/**/*.js')
+  return gulp.src([
+    './src/scripts/**/*.js',
+    './test/**/*.js',
+    './gulpfile.js'
+    ])
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
@@ -15,21 +31,34 @@ gulp.task('sass', function () {
   .pipe(gulp.dest('./src/styles'));
 });
 
-gulp.task('clean', function(callback) {
-  require('del')(['build'], callback);
+gulp.task('test', function (done) {
+  var karma = require('karma').server;
+
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
 });
 
-gulp.task('restore-modules', plugins.shell.task(
+gulp.task('restore-node-dependencies', plugins.shell.task(
   'npm install', {
     cwd: './src'
   }
 ));
 
+gulp.task('restore-bower-dependencies', plugins.shell.task(
+  'bower install'
+));
+
+gulp.task('clean', function(callback) {
+  require('del')(['build'], callback);
+});
+
 gulp.task('run', executeNodeCommand(
   'node_modules/node-webkit-builder/bin/nwbuild --run ./src'
 ));
 
-gulp.task('build', ['clean'], executeNodeCommand(
+gulp.task('build-node-webkit-package', ['clean'], executeNodeCommand(
   'node_modules/node-webkit-builder/bin/nwbuild ./src'
 ));
 
@@ -37,19 +66,24 @@ gulp.task('watch', function() {
   gulp.watch('./src/styles/**/*.scss', ['sass']);
 });
 
+gulp.task('build', function(callback) {
+  runSequence(
+    'restore-node-dependencies', 'restore-bower-dependencies',[
+      'lint',
+      'test',
+      'sass'
+    ], callback);
+});
+
+gulp.task('package', function(callback) {
+  runSequence('build', [
+      'build-node-webkit-package'
+    ], callback);
+});
+
 gulp.task('default', [
-  'restore-modules',
   'lint',
+  'test',
   'sass',
-  'build'
+  'watch'
 ]);
-
-function executeNodeCommand(cmd) {
-  var platform = require('os').platform();
-
-  if (platform === 'win32') {
-    cmd = 'node ' + cmd;
-  }
-
-  return plugins.shell.task(cmd);
-}
---
+++ test/spec/foo.js
@@ -0,0 +1,6 @@
+describe('foo', function() {
+  it('foo should equals bar', function() {
+    var foo = 'bar';
+    expect(foo).to.equal('bar');
+  });
+});
